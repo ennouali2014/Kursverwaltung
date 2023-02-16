430c0391df1a9df9efb0ff5a70c82ca281dd2e9d
Yolla <yhanna@uni-bremen.de>
2023-02-16 15:28:41 +0100

    Repository/KursRepository

---
+++ src/main/java/com/example/kursverwaltung/repository/KursRepository.java
@@ -0,0 +1,9 @@
+package com.example.kursverwaltung.repository;
+
+import com.example.kursverwaltung.domain.Kurs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KursRepository extends JpaRepository<Kurs,Long> {
+}
