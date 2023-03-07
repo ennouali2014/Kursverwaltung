1a905c46a77a4063d22661ee772dc23c934c243c
Yolla <yhanna@uni-bremen.de>
2023-03-07 13:49:39 +0100

    Kurs1 funktioniert einwandfrei

--- src/main/java/com/example/kursverwaltung/controller/Kurs1Controller.java
+++ src/main/java/com/example/kursverwaltung/controller/Kurs1Controller.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -36,25 +35,43 @@ public class Kurs1Controller {
 
     @PostMapping("/savekurs1")
     //public String save(@ModelAttribute("yourModelObject") YourModelClass model)
-    //public String saveKurs1(@ModelAttribute("kurs1") Kurs1 kurs1) {
 
-    public String saveKurs1(@ModelAttribute Kurs1 kurs1, @RequestParam("start_datum1") @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate start_datum1_L ) {
+    public String saveKurs1(@ModelAttribute ("kurs1") Kurs1 kurs1,
+                            @RequestParam("start_datum1") @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate start_datum1_L,
+                            @RequestParam("aktuelle_tn_anzahl1") int aktuelle_tn_anzahl1,
+                            @RequestParam("max_tn_anzahl1") int max_tn_anzahl1,
+                            @RequestParam("gebuehr_brutto1") Double gebuehr_brutto1,
+                            @RequestParam("mwst_prozent1") Double mwst_prozent1) {
+//        if (kurs1 == null || start_datum1_L == null || aktuelle_tn_anzahl1 == null || max_tn_anzahl1 == null) {
+//
+//            return "error";
+//        }
+
         kurs1.setStart_datum1(start_datum1_L);
-        long milliseconds = Math.round((float) kurs1.getAnzahl_tage1() / kurs1.getZyklus1()) * 7 * 86400000L;
+        // long milliseconds = Math.round((float) kurs1.getAnzahl_tage1() / kurs1.getZyklus1()) * 7 * 86400000L;
+        long milliseconds = (Math.round((float) (kurs1.getAnzahl_tage1() / kurs1.getZyklus1()) - 1) * 7L + kurs1.getZyklus1()) * 86400000L;
         kurs1.setEnde_datum1(kurs1.getStart_datum1().plusDays((milliseconds / 86400000L)));
+
+        kurs1.setMax_tn_anzahl1(max_tn_anzahl1);
+        kurs1.setAktuelle_tn_anzahl1(aktuelle_tn_anzahl1);
+        if (kurs1.getMax_tn_anzahl1() >= kurs1.getAktuelle_tn_anzahl1()) {
+            kurs1.setFreie_plaetze1(kurs1.getMax_tn_anzahl1() - kurs1.getAktuelle_tn_anzahl1());
+
+        } else {
+
+            kurs1.setFreie_plaetze1(0);
+        }
+        kurs1.setGebuehr_brutto1(gebuehr_brutto1);
+        kurs1.setMwst_prozent1(mwst_prozent1);
+        kurs1.setGebuehr_netto1(Math.round((kurs1.getGebuehr_brutto1() / (100 + kurs1.getMwst_prozent1()) * 100) * 100.0) / 100.0);
+        kurs1.setMwst_euro1(Math.round((kurs1.getGebuehr_brutto1() / (100 + kurs1.getMwst_prozent1()) * kurs1.getMwst_prozent1()) * 100.0) / 100.0);
+
         service.save(kurs1);
         return "redirect:/k1/kurs1";
-       //(pattern = "dd.MM.yyyy") (iso = DateTimeFormat.ISO.DATE)
+
+        //(pattern = "dd.MM.yyyy") (iso = DateTimeFormat.ISO.DATE)
 
     }  //@ModelAttribute annotation is used to bind the model object from the form submission.
-
-//    @PostMapping("/save")
-//    public String save(@RequestParam("gebuer_netto") Double gebuer_netto,
-//                       @RequestParam("start_date1") @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate start_date1) {
-//        MyEntity entity = new MyEntity(gebuer_netto, start_date1);
-//        repository.save(entity);
-//        return "redirect:/";
-//    }
 
 
     @RequestMapping("/editkurs1/{kurs1Id}")
--- src/main/java/com/example/kursverwaltung/domain/Kurs1.java
+++ src/main/java/com/example/kursverwaltung/domain/Kurs1.java
@@ -19,7 +19,8 @@ public class Kurs1 {
 
     @Column(name = "kursname1", length = 100, nullable = false)
     private String kursname1;
-
+    @Column(name = "status1", length = 100, nullable = false)
+    private String status1;
     @Temporal(TemporalType.DATE)
     @Column(name = "start_datum1", nullable = false)
     private LocalDate start_datum1;
@@ -31,12 +32,43 @@ public class Kurs1 {
     @Temporal(TemporalType.DATE)
     @Column(name = "ende_datum1")
     private LocalDate ende_datum1;
+    @Column(name = "min_tn_anzahl1", nullable = false)
+    private int min_tn_anzahl1;
+    @Column(name = "max_tn_anzahl1", nullable = false)
+    private int max_tn_anzahl1;
+    @Column(name = "freie_plaetze1")
+    private int freie_plaetze1 = 0;
+    @Column(name = "aktuelle_tn_anzahl1", nullable = false)
+    // spaeter wird nicht nullable sondern in add person in kurs Kalkuliert
+    private int aktuelle_tn_anzahl1;
 
-    public Kurs1(String kursname1, LocalDate start_datum1, int anzahl_tage1, int zyklus1) {
+    @Column(name = "gebuehr_brutto1", nullable = false)
+    private Double gebuehr_brutto1;
+
+    @Column(name = "gebuehr_netto1")
+    private Double gebuehr_netto1;
+
+    @Column(name = "mwst_prozent1", nullable = false)
+    private Double mwst_prozent1;
+
+    @Column(name = "mwst_euro1", nullable = false)
+    private Double mwst_euro1;
+
+    @Column(name = "kurs_beschreibung", length = 600, nullable = false)
+    private String kurs_beschreibung;
+
+    public Kurs1(String kursname1, LocalDate start_datum1, int anzahl_tage1, int zyklus1, Double gebuehr_brutto1, Double mwst_prozent1, int min_tn_anzahl1, int max_tn_anzahl1, int aktuelle_tn_anzahl1, String status1,String kurs_beschreibung) {
         this.kursname1 = kursname1;
         this.start_datum1 = start_datum1;
         this.anzahl_tage1 = anzahl_tage1;
         this.zyklus1 = zyklus1;
+        this.gebuehr_brutto1 = gebuehr_brutto1;
+        this.mwst_prozent1 = mwst_prozent1;
+        this.min_tn_anzahl1 = min_tn_anzahl1;
+        this.max_tn_anzahl1 = max_tn_anzahl1;
+        this.aktuelle_tn_anzahl1 = aktuelle_tn_anzahl1;
+        this.status1 = status1;
+        this.kurs_beschreibung=kurs_beschreibung;
     }
 
     public Kurs1() {
@@ -53,7 +85,6 @@ public class Kurs1 {
     public void setKursname1(String kursname1) {
         this.kursname1 = kursname1;
     }
-
 
 
     public LocalDate getStart_datum1() {
@@ -105,4 +136,91 @@ public class Kurs1 {
     public void setEnde_datum1(LocalDate ende_datum1) {
         this.ende_datum1 = ende_datum1;
     }
+
+    public Double getGebuehr_brutto1() {
+        return gebuehr_brutto1;
+    }
+
+    public void setGebuehr_brutto1(Double gebuehr_brutto1) {
+        this.gebuehr_brutto1 = gebuehr_brutto1;
+    }
+
+    public Double getMwst_prozent1() {
+        return mwst_prozent1;
+    }
+
+    public void setMwst_prozent1(Double mwst_prozent1) {
+        this.mwst_prozent1 = mwst_prozent1;
+    }
+
+    public int getMin_tn_anzahl1() {
+        return min_tn_anzahl1;
+    }
+
+    public void setMin_tn_anzahl1(int min_tn_anzahl1) {
+        this.min_tn_anzahl1 = min_tn_anzahl1;
+    }
+
+    public int getMax_tn_anzahl1() {
+        return max_tn_anzahl1;
+    }
+
+    public void setMax_tn_anzahl1(int max_tn_anzahl1) {
+        this.max_tn_anzahl1 = max_tn_anzahl1;
+    }
+
+    public int getAktuelle_tn_anzahl1() {
+        return aktuelle_tn_anzahl1;
+    }
+
+    public void setAktuelle_tn_anzahl1(int aktuelle_tn_anzahl1) {
+        this.aktuelle_tn_anzahl1 = aktuelle_tn_anzahl1;
+    }
+
+    public int getFreie_plaetze1() {
+        return freie_plaetze1;
+    }
+
+    public void setFreie_plaetze1(int freie_plaetze1) {
+        this.freie_plaetze1 = freie_plaetze1;
+    }
+
+    public String getStatus1() {
+        return status1;
+    }
+
+    public void setStatus1(String status1) {
+        this.status1 = status1;
+    }
+
+    public Double getGebuehr_netto1() {
+        return gebuehr_netto1;
+    }
+
+    public void setGebuehr_netto1(Double gebuehr_netto1) {
+        this.gebuehr_netto1 = gebuehr_netto1;
+    }
+
+    public Double getMwst_euro1() {
+        return mwst_euro1;
+    }
+
+    public void setMwst_euro1(Double mwst_euro1) {
+        this.mwst_euro1 = mwst_euro1;
+    }
+
+    public String getKurs_beschreibung() {
+        return kurs_beschreibung;
+    }
+
+    public void setKurs_beschreibung(String kurs_beschreibung) {
+        this.kurs_beschreibung = kurs_beschreibung;
+    }
+    //    public void setFreie_plaetze1(Integer freie_plaetze1) {
+//        if (freie_plaetze1 != null) {
+//            this.freie_plaetze1 = freie_plaetze1;
+//        } else {
+//            this.freie_plaetze1 = 0; // or any other default value you want to use
+//        }
+//    }
 }
