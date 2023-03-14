ea29164076586fdf1f651813d012ac7f61da7815
cody <mpawel@uni-bremen.de>
2023-03-14 11:52:49 +0100

    javadocs

--- src/main/java/com/example/kursverwaltung/domain/Kurs.java
+++ src/main/java/com/example/kursverwaltung/domain/Kurs.java
@@ -3,64 +3,148 @@ package com.example.kursverwaltung.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import javax.validation.constraints.Min;
+
+/**
+ * Die Klasse Kurs definiert die Objekte 'Kurs', mit deren Attributen und Datentypen. (Einige Attribute sind Pflichtangaben, andere werden automatisch berechnet)
+ * Mit Hiberante wird aus der Klasse eine Tabelle in der Datenbank erzeugt, sowie aus den Attributen entsprechende Spalten
+ * Mittels Annotationen werden Verbindungen zur Datenbank hergestellt und auch spezifiziert, wie zb Beziehungen zwischen den Enteties
+ */
 @Entity
 @Table(name = "kurs")
 public class Kurs {
 
+    /**
+     * Id wird automatisch in Datenbank vergeben und nicht vom user
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="kursId")
     private Long kursId;
 
+    /**
+     * Gibt den Namen eines Kurses an, kann mehrfach vorkommen
+     */
     @Column(name = "kursname", length = 100, nullable = false)
     private String kursname;
+
+    /**
+     * Gibt den Status eines Kurses an, zB geplant, wenn der Kurs noch nicht angefangen hat
+     */
     @Column(name = "status", length = 100, nullable = false)
     private String status;
+
+
+    /**
+     * Gibt das Datum an, an dem ein Kurs startet,
+     * hier wird das Datumsformat im englischen Format festgelegt
+     */
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "start_datum", nullable = false)
     private LocalDate start_datum;
 
+    /**
+     * Anzahl_tage legt die gesamte Zahl an Veranstaltungstagen fest
+     */
     @Column(name = "anzahl_tage", nullable = false)
     private int anzahl_tage;
+
+    /**
+     * Zyklus legt die Anzahl der Veranstaltungstage je Woche fest
+     */
     @Column(name = "zyklus", nullable = false)
     private int zyklus;
+
+    /**
+     * Gibt das Datum an, an dem ein Kurs beendet ist,
+     * wird automatisch berechnet
+     * hier wird das Datumsformat im englischen Format festgelegt
+     */
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "ende_datum")
     private LocalDate ende_datum;
+
+    /**
+     * Gibt die Anzahl an Teilnehmern an, die ein Kurs haben muss damit ein Kurs stattfindet
+     */
     @Column(name = "min_tn_anzahl", nullable = false)
     private int min_tn_anzahl;
+
+    /**
+     *  Gibt die Anzahl an Teilnehmern an, die maximal an einem Kurs teilnehmen können
+     */
     @Column(name = "max_tn_anzahl", nullable = false)
     private int max_tn_anzahl;
+
+    /**
+     * Gibt die Zahl an noch verfügbaren Plätzen für potentielle Teilnehmer an,
+     * wird automatisch berechnet
+     */
     @Column(name = "freie_plaetze")
     private int freie_plaetze = 0;
 
+    /**
+     * Gibt die aktuelle Anzahl an Teilnehmern eines Kurses an,
+     * wird automatisch berechnet
+     */
    @Column(name = "aktuelle_tn_anzahl")
     private int aktuelle_tn_anzahl;
 
+    /**
+     * Gibt die gesamte Gebuehr für eine Kursteilnahme an, inklusive Mehrwertsteuer
+     */
     @Column(name = "gebuehr_brutto", nullable = false)
     private Double gebuehr_brutto;
 
+    /**
+     * Gibt die gesamte Gebuehr für eine Kursteilnahme an, exkusive MEhrwertsteuer
+     * wird automatisch berechnet
+     */
     @Column(name = "gebuehr_netto")
     private Double gebuehr_netto;
 
+    /**
+     * Gibt den Prozentsatz der Mehrwertsteuer einer Kursgebuehr an,
+     * kann vom User festgelegt werden
+     */
     @Column(name = "mwst_prozent", nullable = false)
     private Double mwst_prozent;
 
+    /**
+     * Gibt die Mehrwertsteuer einer Kursgebuehr in Euro an
+     * wird automatisch berechnet
+     */
     @Column(name = "mwst_euro", nullable = false)
     private Double mwst_euro;
 
+    /**
+     * Bietet Raum für ausfuehrliche Informationen ueber einen Kurs
+     */
     @Column(name = "kurs_beschreibung", length = 600, nullable = false)
     private String kurs_beschreibung;
 
+    /**
+     * Die Interessenten an einem Kurs werden in einer Hash-Tabelle gespeichert, wodurch ein schneller Zugriff möglich ist.
+     */
     @ManyToMany(mappedBy = "inKursinteressieren")
     @JsonIgnore
     private Set<Person> interessant = new HashSet<>();
 
+
+    /**
+     * Die Teilnehmer an einem Kurs werden in einer Hash-Tabelle gespeichert, wodurch ein schneller Zugriff möglich ist.
+     */
     @ManyToMany(mappedBy = "inKursteilnehmen")
     @JsonIgnore
     private Set<Person> teilnehmer = new HashSet<>();
@@ -78,6 +162,10 @@ public class Kurs {
         this.status = status1;
         this.kurs_beschreibung=kurs_beschreibung;
     }
+
+    /**
+     * Dieser Konstruktor hat keine Parameter wg hibernate
+     */
     public Kurs() {
     }
 
@@ -208,6 +296,8 @@ public class Kurs {
     }
 
 
+
+
     public void setKursId(Long kursId) {
         this.kursId = kursId;
     }
@@ -235,6 +325,13 @@ public class Kurs {
     public void hadInteressant(Person person) {
         person.schonInteressant(this);
     }
+
+    /**
+     * Methode add wird genutzt um eine Person hinzuzufügen, wird im KursController und im PersonenController eingesetzt,
+     * @param person
+     * @param personSet
+     * @return
+     */
     public Set<Person> add(Person person,Set<Person> personSet){
         for (Person p: personSet){
             if(p.getPersonId().equals(person.getPersonId())){
